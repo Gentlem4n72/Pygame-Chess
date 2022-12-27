4a1fc3af04c0c536bf57b35b66b75d9e1203fb46
ScaryOrange <UnKindledastor@yandex.ru>
2022-12-27 14:44:24 +0300

    fakcing ball sheet

--- chess.py
+++ chess.py
@@ -169,6 +169,7 @@ class Board(pygame.sprite.Sprite):
                                     rook.rect.x, piece.rect.y = get_pixels((col1 - 3, row1))
                                 self.color = opponent(self.color)
                                 piece.turn += 1
+                                check(self)
                                 return True
                             else:
                                 return False
@@ -180,6 +181,7 @@ class Board(pygame.sprite.Sprite):
                     self.color = opponent(self.color)
                     if piece.char() == 'K':
                         piece.turn += 1
+                    check(self)
                     return True
 
 
@@ -202,20 +204,19 @@ class Rook(pygame.sprite.Sprite):
         return 'R'
 
     def can_move(self, board, row, col, row1, col1):
-        # Невозможно сделать ход в клетку, которая не лежит в том же ряду
-        # или столбце клеток.
+        if row1 == row and col1 == col:
+            return False
+
         if row != row1 and col != col1:
             return False
 
         step = 1 if (row1 >= row) else -1
         for r in range(row + step, row1, step):
-            # Если на пути по горизонтали есть фигура
             if not (board.get_piece(r, col) is None):
                 return False
 
         step = 1 if (col1 >= col) else -1
         for c in range(col + step, col1, step):
-            # Если на пути по вертикали есть фигура
             if not (board.get_piece(row, c) is None):
                 return False
 
@@ -244,13 +245,9 @@ class Pawn(pygame.sprite.Sprite):
         return 'P'
 
     def can_move(self, board, row, col, row1, col1):
-        # Пешка может ходить только по вертикали
-        # "взятие на проходе" не реализовано
         if col != col1:
             return False
 
-        # Пешка может сделать из начального положения ход на 2 клетки
-        # вперёд, поэтому поместим индекс начального ряда в start_row.
         if self.color == WHITE:
             direction = 1
             start_row = 1
@@ -258,11 +255,9 @@ class Pawn(pygame.sprite.Sprite):
             direction = -1
             start_row = 6
 
-        # ход на 1 клетку
         if row + direction == row1:
             return True
 
-        # ход на 2 клетки из начального положения
         if (row == start_row
                 and row + 2 * direction == row1
                 and board.field[row + direction][col] is None):
@@ -271,6 +266,8 @@ class Pawn(pygame.sprite.Sprite):
         return False
 
     def can_attack(self, board, row, col, row1, col1):
+        if row1 == row and col1 == col:
+            return False
         direction = 1 if (self.color == WHITE) else -1
         return (row + direction == row1
                 and (col + 1 == col1 or col - 1 == col1))
@@ -297,6 +294,8 @@ class Knight(pygame.sprite.Sprite):
     def can_move(self, board, row, col, row1, col1):
         delta_row = abs(row1 - row)
         delta_col = abs(col1 - col)
+        if row1 == row and col1 == col:
+            return False
         if (delta_row == 2 and delta_col == 1 or
                 delta_row == 1 and delta_col == 2):
             return True
@@ -327,6 +326,11 @@ class King(pygame.sprite.Sprite):
         delta_row = abs(row - row1)
         delta_col = abs(col - col1)
         if delta_row <= 1 and delta_col <= 1:
+            if any(map(lambda x: x.can_attack(board, *get_cell((x.rect.y, x.rect.x)),
+                                              *((row1, col1) if isinstance(x, Queen) or isinstance(x, Bishop) else (col1, row1))),
+                       filter(lambda x: x.color == opponent(self.color),
+                              [x for x in all_pieces.sprites()]))):
+                return False
             return True
         return False
 
@@ -353,6 +357,8 @@ class Queen(pygame.sprite.Sprite):
     def can_move(self, board, row, col, row1, col1):
         delta_row = abs(row1 - row)
         delta_col = abs(col1 - col)
+        if row1 == row and col1 == col:
+            return False
         if row == row1 or col == col1:
             step = 1 if (row1 >= row) else -1
             for r in range(row + step, row1, step):
@@ -400,6 +406,9 @@ class Bishop(pygame.sprite.Sprite):
         delta_row = abs(row1 - row)
         delta_col = abs(col1 - col)
 
+        if row1 == row and col1 == col:
+            return False
+
         if delta_row != delta_col:
             return False
 
@@ -432,8 +441,6 @@ if __name__ == "__main__":
                 if (not (board.field[y][x] is None) and
                         board.color == board.field[y][x].get_color()):
                     board.move_piece(x, y)
-                    if check(board):
-                        running = False
         all_sprites.update()
         all_sprites.draw(screen)
         pygame.display.flip()
